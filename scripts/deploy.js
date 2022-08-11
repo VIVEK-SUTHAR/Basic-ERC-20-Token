b5b640d49c3abf4cbd36e74c02e958a2abf25f2c
VIVEK <sutharvivek18@gmail.com>
2022-08-11 16:49:24 +0530

    fea : local testing added for transfer of Tokens

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,12 +10,12 @@ async function main() {
     const ADDRESS = devToken.address;
     const balaceBeforeTranfer = await devToken.balanceOf("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266");
     console.log("MY BALANCE BEFORE TRANSFER " + parseInt(balaceBeforeTranfer));
-    const tra = await devToken.transfer("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC", 100);
+    const tra = await devToken.transfer("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC", 180);
     const balaceAfterTranfer = await devToken.balanceOf("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266");
     console.log("MY BALANCE AFTER TRANSFER " + parseInt(balaceAfterTranfer));
 
     const transferedTo = await devToken.balanceOf("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
-    console.log("Token balance of address tranferred to " + parseInt(transferedTo));
+    console.log("Token balance of 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC : " + parseInt(transferedTo));
 }
 
 main()
