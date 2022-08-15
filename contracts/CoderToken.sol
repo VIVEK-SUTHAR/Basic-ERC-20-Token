82e76bd7af1bf7b44b304c9ebe21806d6ae1efc1
VIVEK <sutharvivek18@gmail.com>
2022-08-15 21:52:53 +0530

    fea : Test Added

--- contracts/CoderToken.sol
+++ contracts/CoderToken.sol
@@ -3,7 +3,15 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CoderToken is ERC20 {
-    constructor(uint256 totalSupply) ERC20("CoderToken", "CDTK") {
-        _mint(msg.sender, totalSupply);
+    uint256 _totalSupply = 1000;
+    uint256 _currentSupply = 0;
+
+    constructor() ERC20("CoderToken", "CDTK") {}
+
+    function mintToken(uint256 ammount) public {
+        _currentSupply = _currentSupply + ammount;
+        require(_currentSupply <= _totalSupply, "toral Supplyt Exceddded");
+        _mint(msg.sender, ammount);
+        
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,23 +1,19 @@
 const hre = require("hardhat");
-
 async function main() {
     const CoderToken = await hre.ethers.getContractFactory("CoderToken");
-    const devToken = await CoderToken.deploy("1024");
-
+    const devToken = await CoderToken.deploy();
     await devToken.deployed();
-
     console.log("Token deployed to:", devToken.address);
-    const ADDRESS = devToken.address;
+    const mintToken=await devToken.mintToken(900);
     const balaceBeforeTranfer = await devToken.balanceOf("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266");
     console.log("MY BALANCE BEFORE TRANSFER " + parseInt(balaceBeforeTranfer));
     const tra = await devToken.transfer("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC", 180);
     const balaceAfterTranfer = await devToken.balanceOf("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266");
     console.log("MY BALANCE AFTER TRANSFER " + parseInt(balaceAfterTranfer));
-
     const transferedTo = await devToken.balanceOf("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC");
-    console.log("Token balance of 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC : " + parseInt(transferedTo));
+    console.log("Token balance of 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC : " + parseInt(transferedTo));4
+    const mintAnothet=await devToken.mintToken(200);
 }
-
 main()
     .then(() => process.exit(0))
     .catch((error) => {
